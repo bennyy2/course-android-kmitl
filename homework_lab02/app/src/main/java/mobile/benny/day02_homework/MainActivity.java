f1085514cf19d09fc2685fd3aa2488673cd29b37
bennyy2 <30916980+bennyy2@users.noreply.github.com>
2017-08-22 21:46:02 +0700

    submit homework for lab02 day 18-8-60

---
+++ homework_lab02/app/src/main/java/mobile/benny/day02_homework/MainActivity.java
@@ -0,0 +1,13 @@
+package mobile.benny.day02_homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_store);
+    }
+}
